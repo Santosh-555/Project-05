320201067a80416d581c0d61268aa638902999df
R Santosh Kumar Mudaliar <santoshmudaliar555@gmail.com>
2024-06-10 19:55:40 +0530

    Initial commit

---
+++ Book Store/backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const MONGO_DB_URL =
+  "mongodb+srv://santoshkumarmudaliar555:6Uel8TX3IUFzpZsR@project-05-book-store.ugezwzm.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Project-05-Book-Store";
---
+++ Book Store/frontend/src/components/home/BookModal.jsx
@@ -0,0 +1,47 @@
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable react/prop-types */
+
+import { AiOutlineClose } from "react-icons/ai";
+import { PiBookOpenTextLight } from "react-icons/pi";
+import { BiUserCircle } from "react-icons/bi";
+
+const BookModal = ({ book, onClose }) => {
+  return (
+    <div
+      className="fixed bg-black bg-opacity-60 top-0 left-0 right-0 bottom-0 z-50 flex justify-center items-center"
+      onClick={onClose}
+    >
+      <div
+        onClick={(event) => event.stopPropagation()}
+        className="w-[600px] max-w-full h-[400px] bg-white rounded-xl p-4 flex flex-col relative"
+      >
+        <AiOutlineClose
+          className="absolute right-6 top-6 text-3xl text-red-600 cursor-pointer"
+          onClick={onClose}
+        />
+        <h2 className="w-fit px-4 py-1 bg-red-300 rounded-lg">
+          {book.publishYear}
+        </h2>
+        <h4 className="my-2 text-gray-500">{book._id}</h4>
+        <div className="flex justify-start items-center gap-x-2">
+          <PiBookOpenTextLight className="text-red-300 text-2xl" />
+          <h2 className="my-1">{book.title}</h2>
+        </div>
+        <div className="flex justify-start items-center gap-x-2">
+          <BiUserCircle className="text-red-300 text-2xl" />
+          <h2 className="my-1">{book.author}</h2>
+        </div>
+        <p className="mt-4">Samrudhi and the Symphony of Dreams</p>
+        <p className="my-2">
+          In a serene village, Samrudhi, a dreamer, lived amidst nature's
+          embrace, her heart a sanctuary of desires. Every night, beneath the
+          moon's gentle light, her dreams intertwined with the universe. With
+          boundless hope, she brought her wishes to life, her journey a symphony
+          of love, igniting belief in the enchantment within every soul.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default BookModal;
